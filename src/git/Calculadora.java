02cdde435b23d7119225de46b4aae80bc56d588e
PacchiDavid <pachoncitox@gmail.com>
2021-04-18 14:39:36 -0300

    cambio orden switch

--- src/git/Calculadora.java
+++ src/git/Calculadora.java
@@ -10,7 +10,7 @@ public class Calculadora {
 		 Integer resultado;
 		 do{
 		 System.out.println
-		 ("Ingrese su operación /n 1 para sumar /n 2 para restar /n 3 para multiplicar /n 4 para dividir");
+		 ("Ingrese su operación /n 1 para dividir /n 2 para multiplicar /n 3 para restar /n 4 para sumar");
 		 opcion = teclado.next();
 		 }while(opcion < 1 && opcion > 4)
 		 System.out.println("Ingrese el primer numero");
@@ -18,32 +18,28 @@ public class Calculadora {
 		 System.out.println("Ingrese el segundo numero");
 		 a = teclado.next();
 		 
-		 
 		 switch(opcion) {
 		 case 1 :
-			System.out.println("elegiste la opcion sumar");
-			resultado = a +b ;
-			break;
+			 	System.out.println("elegiste la opcion dividir");
+			 	resultado = a / b ;
+			 	break;
+		
 		 case 2 :
-				System.out.println("elegiste la opcion restar");
+			 	System.out.println("elegiste la opcion multiplicar");
+				resultado = a * b ;
+				break;
+				
+		 case 3 :
+			 	System.out.println("elegiste la opcion restar");
 				resultado = a - b ;
 				break;
 			 
-		 case 3 :
-				System.out.println("elegiste la opcion multiplicar");
-				resultado = a * b ;
-				break;
-		 case 4 :
-				System.out.println("elegiste la opcion dividir");
-				resultado = a / b ;
-				break;
-			 
-			 	
-		
+		 case 4 :		
+			 	System.out.println("elegiste la opcion sumar");
+				resultado = a +b ;
+				break;	
 		 }
-		 
-		 
-		 System.out.println("El resultado es " + resultado.toString());
+		 	System.out.println("El resultado es " + resultado.toString());
 		 }
 		}
 
